282a9e9a368d1c8efa66811d7e6984a45ce72006
Takafumi Hirata <takhirata6@gmail.com>
2018-04-08 23:24:51 +0900

    init

---
+++ development.js
@@ -0,0 +1,41 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const src = path.resolve(__dirname, 'src');
+const dist = path.resolve(__dirname, 'public');
+
+export default {
+  entry: src + '/index.jsx',
+
+  output: {
+    path: dist,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: true
+          }
+        },
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.jsx', '.js']
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: src + '/index.html',
+      filename: 'index.html'
+    })
+  ],
+  mode: "development"
+}
